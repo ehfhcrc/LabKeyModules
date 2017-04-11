6df09fd14109c47898a1523fa9826709e382dadf
ehfhcrc <ehenrich@fredhutch.org>
2017-04-11 11:47:22 -0700

    added popup

--- HIPCMatrix/web/HIPCMatrix/CreateMatrix.js
+++ HIPCMatrix/web/HIPCMatrix/CreateMatrix.js
@@ -5,17 +5,18 @@ function createMatrixClick(dataRegion)
     var schemaName = dataRegion.schemaName;
     var queryName = dataRegion.queryName;
     var selectionKey = dataRegion.selectionKey;
-
     var cohorts = dataRegion.getColumn("Cohort")
+    
     function onlyUnique(value, index, self) { 
     	return self.indexOf(value) === index;
 	}
+
 	var uniqueCohorts = cohorts.filter( onlyUnique )
 
 	/* Ensure only one cohort, otherwise names are concatenated and are not found
 	by various schema since they are not in the arm_2_cohort table */
-	if( cohorts > 1 ) {
-		/* popup */
+	if( uniqueCohorts > 1 ) {
+		alert("Please redo selection to use only one cohort.")
 	} else {
 		window.location = LABKEY.ActionURL.buildURL("HIPCMatrix", "CreateMatrix.view", null, {
 	        schemaName: schemaName,
